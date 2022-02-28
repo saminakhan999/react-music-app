2d619c96d933153e42d7e0d420cc75ff204e08a8
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-28 20:59:05 +0000

    add test folder

---
+++ src/test/app.test.js
@@ -0,0 +1,13 @@
+import { screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('App', () => {
+
+    beforeEach(() => {
+      render(<App />);
+    });
+
+    
+
+})
