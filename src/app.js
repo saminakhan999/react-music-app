92dc66a77acaabbf2be1f2ef70dda33e7dbad5d0
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-28 13:07:38 +0000

    update app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Music from './components/Music';
+
+function App() {
+    return (
+      <main>
+        <h1>Personal Favourites Music List</h1>
+        <Music />
+      </main>
+    );
+  }
+  
+  export default App;
