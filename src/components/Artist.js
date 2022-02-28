06b7cfe437a6457a0d4c35b7702093b26c02702e
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-28 20:58:23 +0000

    add korean writing

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Artist = () => {
     
     return (
-        <div>
-          <h1>NCT</h1>
-          <h3>K-Pop Group</h3>
-          <p>NCT, also known as Neo-Culture Technology, is a K-Pop boy group consisting of 23 members. These members are split into subgroups called NCT 127, NCT Dream and WayV - each with their own distinct theme.</p>
+        <div style={{ width: "90vw", textAlign: "center", margin: "auto" }}>
+          <h1 className={"info-header"}>엔시티</h1>
+          <h3 className={"info-subheader"}>K-Pop Group</h3>
+          <p className={"info-para"}>NCT, also known as Neo-Culture Technology, is a K-Pop boy group consisting of 23 members. These members are split into subgroups called NCT 127, NCT Dream and WayV - each with their own distinct theme.</p>
         </div>
       );
 }
