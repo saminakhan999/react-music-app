9c0d9d3449662ad2b6184216e1ffe0faea2eb5b1
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-02 14:39:09 +0000

    update nav bar and header

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -4,14 +4,17 @@ import './index.css';
 
 const Header = () => {
     return (
-        <>
-        <h1 className={"main-header"}>엔시티</h1>
-        <nav>
-            <NavLink exact to="/" activeClassName="current">Home</NavLink>
-            <NavLink to="/listen" activeClassName="current">About</NavLink>
-            <NavLink to="/jokes" activeClassName="current">Jokes</NavLink>
-        </nav>
-        </>
+        <div className={"whole-ting"}>
+            <img className={"hand-image"} src={"https://wallpapercave.com/wp/wp4952454.jpg"}></img>
+            <h1 className={"main-header"}>엔시티</h1>
+            <nav>
+                <NavLink exact to="/" activeClassName="current">Home</NavLink>
+                <NavLink to="/about" activeClassName="current">About</NavLink>
+                <NavLink to="/favourite" activeClassName="current">Favourites</NavLink>
+                <NavLink to="/listen" activeClassName="current">Listen</NavLink>
+                <NavLink to="/jokes" activeClassName="current">Jokes</NavLink>
+            </nav>
+        </div>
     );
 }
 
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,8 +5,7 @@ import './index.css';
 function Main () {
     return (
       <main>
-        <img className={"hand-image"} src={"https://wallpapercave.com/wp/wp4952454.jpg"}></img>
-        <h1 className={"main-header"}>Favourite Music by NCT</h1>
+        <h1 className={"fave-music-header"}>Favourite Music by NCT</h1>
         <Artist />
         <SongList />
       </main>
