21330ed0fb290abfb022d29d9e3c73a7fff1901c
saminakhan999 <samina.khan9999@gmail.com>
2022-03-02 20:18:10 +0000

    added images:

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,8 @@ import './index.css';
 function Main () {
     return (
       <main>
-        <h1>WELCOME</h1>
+        <h1>WELCOME NCTZENS</h1>
+        <img className={'main-img'} src="https://nowcdn.smtown.com/upload/news/images/2021/05/07/7AhMiEMWadDTe91E_180e02717be24d8090cfcd0e2327a833_1620356481618.jpg"></img>
       </main>
     );
   }
