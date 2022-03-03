5129c01c54f0adadf58103446ac8a7658a799cc0
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-03 19:32:05 +0000

    add more music

--- src/components/MusicButtons/index.js
+++ src/components/MusicButtons/index.js
@@ -7,6 +7,11 @@ import DOMINO from './Audio/WAYV-DOMINO.mp3'
 // import COW from "./Audio/COW.mp3";
 // import SHEEP from "./Audio/SHEEP.mp3";
 import CHERRYBOMB from "./Audio/NCT-127-CHERRY-BOMB.mp3";
+import LIMITLESS from "./Audio/NCT-127-LIMITLESS.mp3";
+import STICKER from "./Audio/NCT-127-STICKER.mp3";
+import LEMONADE from "./Audio/NCT-127-LEMONADE.mp3";
+import SIMONSAYS from "./Audio/NCT-127-SIMON-SAYS.mp3";
+import TURNBACKTIME from "./Audio/WAYV-TURN-BACK-TIME.mp3";
 
 const MusicButton = () => {
     
@@ -14,6 +19,11 @@ const MusicButton = () => {
       { id: 1, song: "BOSS", link: BOSS, artistName: "NCT", songName: "boss" },
       { id: 2, song: "Domino", link: DOMINO, artistName: "WAYV", songName: "domino" },
       { id: 3, song: "Cherry Bomb", link: CHERRYBOMB, artistName: "NCT", songName: "cherry-bomb"},
+      { id: 4, song: "Limitless", link: LIMITLESS, artistName: "NCT", songName: "limitless"},
+      { id: 5, song: "Sticker", link: STICKER, artistName: "NCT", songName: "sticker"},
+      { id: 6, song: "Lemonade", link: LEMONADE, artistName: "NCT", songName: "lemonade"},
+      { id: 7, song: "Simon Says", link: SIMONSAYS, artistName: "NCT", songName: "simon-says"},
+      { id: 8, song: "Turn Back Time", link: TURNBACKTIME, artistName: "WAYV", songName: "turn-back-time"},
     ]);
 
     ///////////////////////////////////////////////////////////////////////////////
@@ -60,7 +70,6 @@ const MusicButton = () => {
             setStatusMessage('Loading')
             try {   
                 let { data } =  chosenButton && await axios.get(`https://api.lyrics.ovh/v1/${chosenButton.artistName}/${chosenButton.songName}`);
-                console.log(data)
                 setLyric(data);
                 setStatusMessage('');
             } catch (err) {
