22e91d205b9579f83942792c76a2c32effcacb88
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-03 19:32:48 +0000

    change route

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/favourite">
+          <Route path="/songs">
             <Favourites />
           </Route>
           <Route path="/listen">
