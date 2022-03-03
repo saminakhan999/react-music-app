c0041a5030d45ac3bd6b8b67836ab3fba3c24f94
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-03 10:41:41 +0000

    update

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { default as Artist } from './Artist'
 export { default as SongList } from './Music'
 export { default as MusicButton } from './MusicButtons'
 export { default as Lyrics } from "./Lyrics";
-export { default as FaveButton } from "./FaveButton";
 export { default as BackButton } from "./BackButton";
