2e6c235b32584e74b0760f203b10d6cb38b9fbbb
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-03 10:10:28 +0000

    update footer

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import Speech from 'react-speech';
 import './index.css';
 
-const Footer = () => <span id="footer">&copy; MAHARANI INC.</span>
+const Footer = () => {
+    return (
+    <>
+        <span id="footer">&copy; MAHARANI INC. 
+        <div className="speech">
+            <Speech text="Maharani Inc is really cool" />
+        </div>
+        </span>
+    </>
+    )
+}
 
 export default Footer;
